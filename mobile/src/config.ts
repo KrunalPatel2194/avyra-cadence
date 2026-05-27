c701d8f4efd507a12cc781adf376fbba30262f01
Krunal Patel <krunalp2102@gmail.com>
2026-05-27 00:28:10 -0400

    auth: web client moved to same project as iOS client

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -10,7 +10,7 @@ const googleIosClientId = "226986045351-11sa15i7d2dlhla6l7tfo2a8ommp558v.apps.go
 // Used by the SDK to get a `serverAuthCode` that cadence-api exchanges (with
 // the Web client secret) for a refresh token. Create one in Google Console:
 // Application type = Web application; leave redirect URIs empty.
-const googleWebClientId = "417755717114-09qf0gimrafdq02s6e71b49sq5coub65.apps.googleusercontent.com";
+const googleWebClientId = "226986045351-f10auifede8kpov7oc2d0mtoeeivqe5c.apps.googleusercontent.com";
 
 export const config = {
   apiUrl,
